c3174ec8cd93d470b1d6f0ef184adaa95bad1872
lordnyks <lordnyks@gmail.com>
2021-03-19 10:59:38 +0200

    my first commit

---
+++ src/main/java/com/monitoring/documents/MonitoringDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.monitoring.documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringDocumentsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/monitoring/documents/services/UserService.java
@@ -0,0 +1,12 @@
+package com.monitoring.documents.services;
+
+import com.monitoring.documents.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+}
